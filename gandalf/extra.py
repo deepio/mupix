e9c6e8a515a2eb7b2675f65d5deaa845f2886127
deepio <alex.daigle2@gmail.com>
2019-05-03 17:37:28 -0400

    Added: Handle backup

--- gandalf/extra.py
+++ gandalf/extra.py
@@ -22,3 +22,31 @@ def boundary_search(begin, end, string):
   """
   pattern = f"(?<={begin})(.*?)(?={end})"
   return re.findall(pattern, string, re.DOTALL)
+
+
+def handle_backups(file_path):
+  """
+  Handle Backups
+
+  In MusicXML, one way to write multiple notes on the same beat in a measure is by adding a `<backup>`
+  tag. This tag will literally "backup the placement" by a defined duration (also called duration). To avoid this,
+  i'm renaming the duration tag and placing the backup with it's associated note prior to parsing.
+
+  Args:
+    file_path (string): A string representation of the filename path.
+
+  Returns:
+    (string): Corrected contents of the file as a long string.
+  """
+  with open(file_path) as f:
+    data = f.readlines()
+
+  # This assumes the backup tag is always 3 lines long.
+  note_back_ups = [index for index, x in enumerate(data) if x == "<backup>\n"]
+  for note_back_up in note_back_ups:
+    # Shuffle all important lines forward
+    data[note_back_up + 2], data[note_back_up - 1], data[note_back_up], data[note_back_up + 1] = data[note_back_up - 1], data[note_back_up], data[note_back_up + 1].replace("duration", "vertical-alignment"), data[note_back_up + 2] # noqa E501
+
+  return "".join(data)
+
+
