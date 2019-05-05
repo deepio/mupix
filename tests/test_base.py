5f1dd585428fd90ede9573b2c67155c3352f2987
deepio <alex.daigle2@gmail.com>
2019-05-04 23:30:37 -0400

    Added: Parser tests

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,6 +1,10 @@
+import hashlib
+import json
+
 import pytest
 
 from gandalf.base import ValidateXML
+from gandalf.base import MusicXML_Parser
 
 test_file = "tests/xml/test.xml"
 schema = "tests/xml/musicxml.xsd"
@@ -11,3 +15,30 @@ def test_validate_xml():
   global test_file, schema
   assert ValidateXML(schema, test_file).isvalid()
 
+
+def test_musicxml_parser():
+  global test_file
+
+  with open(test_file) as f:
+    data = MusicXML_Parser(f.read())
+
+  def test_vendor(data):
+    assert data.get_vendor() == "Sibelius 7.1.3"
+
+  def test_instruments(data):
+    assert data.get_instruments() == ['P1-I1', 'P2-I1', 'P3-I1', 'P4-I1', 'P5-I1', 'P6-I1', 'P7-I1']
+
+  def test_parts(data):
+    m = hashlib.sha512()
+    m.update(bytes(json.dumps(data.get_parts()), "utf-8"))
+    correct_hash = b"\xcc\xc4\xbbX\x89o\xe9+s\xebkkS\x1e\x7f\xa7\xc4\xacAZ\xae\x02\x08\x99\xfeD,\x12\x92t\x17\xbe\xcc"
+    correct_hash += b"\x13\xa8\xe4\xb8\xc2(nP\xd0\xbcz@\xd9\xcd\xeb\xb4\x0f\xec?\xfb\xf9\xb9th32PX\xca\x02\x83"
+    assert m.digest() == correct_hash
+
+  test_vendor(data)
+  test_instruments(data)
+  test_parts(data)
+
+
+if __name__ == "__main__":
+  test_musicxml_parser()
