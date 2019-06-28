11ed4c817cdaa8f94e1e4d317f4e49332cdb5d0f
deepio <alex.daigle2@gmail.com>
2019-06-28 00:45:27 -0400

    Modified: conftest style to reflect project

--- conftest.py
+++ conftest.py
@@ -1,27 +1,28 @@
+import multiprocessing
 import sys
 
 import pytest
 
+from gandalf.application import Compare
+from gandalf.extra import __return_root_path
+
 
 def pytest_addoption(parser):
-    parser.addoption(
-        "--runslow", action="store_true", default=False, help="run slow tests"
-    )
-
-
-def pytest_load_initial_conftests(args):
-    if "xdist" in sys.modules:  # pytest-xdist plugin
-        import multiprocessing
-
-        num = max(multiprocessing.cpu_count() / 2, 1)
-        args[:] = ["-n", str(num)] + args
+  parser.addoption("--runslow", action="store_true", default=False, help="run slow tests")
 
 
 def pytest_collection_modifyitems(config, items):
-    if config.getoption("--runslow"):
-        # --runslow given in cli: do not skip slow tests
-        return
-    skip_slow = pytest.mark.skip(reason="need --runslow option to run")
-    for item in items:
-        if "slow" in item.keywords:
-            item.add_marker(skip_slow)
+  if config.getoption("--runslow"):
+    # --runslow given in cli: do not skip slow tests
+    return
+  skip_slow = pytest.mark.skip(reason="need --runslow option to run")
+  for item in items:
+    if "slow" in item.keywords:
+      item.add_marker(skip_slow)
+
+
+def pytest_load_initial_conftests(args):
+  if "xdist" in sys.modules:  # pytest-xdist plugin
+    num = max(multiprocessing.cpu_count() / 2, 1)
+    args[:] = ["-n", str(num)] + args
+
