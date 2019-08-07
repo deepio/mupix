911e65b628cb17b3e1059ac6a81981a3a4792ecc
deepio <alex.daigle2@gmail.com>
2019-08-07 17:05:35 -0400

    Updated: Result objects docs

--- gandalf/result_objects.py
+++ gandalf/result_objects.py
@@ -3,86 +3,130 @@ import attr
 
 @attr.s
 class Result:
+  """
+  Base results object that holds the number of right and wrong notes.
+
+  Arg
+    right (int): The number of correct Gandalf Object Elements
+    wrong (int): The number of wrong Gandalf Object Elements
+    name (str): The name this results object
+  """
   right = attr.ib(init=False, type=int, default=0)
   wrong = attr.ib(init=False, type=int, default=0)
 
   name = attr.ib(type=str)
   @name.default
   def _get_name(self):
+    """
+    Get the name of this Results object, even when it's inherited so its still available 
+    when calling an attrs as a dictionary.
+    """
     return str(self.__class__.__name__)
 
   def asdict(self):
+    """
+    This is a just a method to unify. I'd rather have a method to turn a class object 
+    into a dictionary, instead of how attrs works like this.
+    """
     return attr.asdict(self)
 
 
 @attr.s
 class NotePitchResult(Result):
-  pass
+  """
+  Results Object that holds the number of correct and incorrect Note Pitches.
+  """
 
 
 @attr.s
 class NoteDurationResult(Result):
-  pass
+  """
+  Results Object that holds the number of correct and incorrect Note Durations.
+  """
 
 
 @attr.s
 class NoteOctaveResult(Result):
-  pass
+  """
+  Results Object that holds the number of correct and incorrect Note Octaves.
+  """
 
 
 @attr.s
 class NoteAccidentalResult(Result):
-  pass
+  """
+  Results Object that holds the number of correct and incorrect Note Accidentals
+  """
 
 
 @attr.s
 class NoteStemDirectionResult(Result):
-  pass
+  """
+  Results Object that holds the number of correct and incorrect Note Stem Directions
+  """
 
 
 @attr.s
 class NoteBeamResult(Result):
-  pass
+  """
+  Results Object that holds the number of correct and incorrect Note Beams
+  """
 
 
 @attr.s
 class NoteTotalResult(Result):
-  pass
+  """
+  The accumulated total of all the Note related Results Objects
+  """
 
 
 @attr.s
 class RestAccidentalResult(Result):
-  pass
+  """
+  I remeber tthere was a reason why this was here, but I can not
+  for the life of me remember what that was.
+  """
 
 
 @attr.s
 class RestDurationResult(Result):
-  pass
+  """
+  Results Object that holds the number of correct and incorrect Rest Durations
+  """
 
 
 @attr.s
 class RestTotalResult(Result):
-  pass
+  """
+  The accumulated total of all the Rest related Results Objects
+  """
 
 
 @attr.s
 class TimeSignatureNumeratorResult(Result):
-  pass
+  """
+  Results Object that holds the number of correct and incorrect Time Signature Numerator values
+  """
 
 
 @attr.s
 class TimeSignatureDenominatorResult(Result):
-  pass
+  """
+  Results Object that holds the number of correct and incorrect Time Signature Denominator values
+  """
 
 
 @attr.s
 class TimeSignatureTotalResult(Result):
-  pass
+  """
+  The accumulated total of all the Time Signature related Results Objects
+  """
 
 
 @attr.s
 class KeySignatureStepResult(Result):
-  pass
+  """
+  """
 
 
 @attr.s
