bf4f580ac558b0d7ff926b6a9c03a2fc22d7f7c5
deepio <alex.daigle2@gmail.com>
2019-09-29 13:00:11 -0400

    Added: Boundary search

--- gandalf/extra.py
+++ gandalf/extra.py
@@ -1,5 +1,6 @@
 import json
 import os
+import re
 
 
 def __return_root_path():
@@ -22,6 +23,23 @@ def return_char_except(value: int) -> str:
   if value >= 95:
     value += 1
   return chr(value)
+
+def boundary_search(begin, end, string):
+  """
+  Boundary Search
+  This is the regex formula for finding everything between two specific strings.
+  e.g. :
+        this character =>[ (all of the stuff here, including newlines) ] <= this character
+        (?<=[)(.*?)(?=])
+  Args:
+    begin (string): The character that signals the begins what you want to search for.
+    end (string): The character that signals the end of what you are looking for.
+    string (string): Multi-line data you wish to search through.
+  Returns:
+    output (list): A list of all instances of what was searched for.
+  """
+  pattern = f"(?<={begin})(.*?)(?={end})"
+  return re.findall(pattern, string, re.DOTALL)
 
 
 def output_filter(ctx, func, *args, **kwargs):
