ac2a6b76139823718ba51974a3f2f456b92ca9a7
deepio <alex.daigle2@gmail.com>
2019-10-28 23:49:21 -0400

    .

--- gandalf/extra.py
+++ gandalf/extra.py
@@ -52,7 +52,6 @@ def output_filter(ctx, func, *args, **kwargs):
   msg = {}
   # If no filtering options are defined, output all information
   if all(value == False for value in [ctx["n"], ctx["r"], ctx["t"], ctx["k"], ctx["c"], ctx["z"]]):  # noqa
-    raise Exception(f"Hmm {ctx.values()} {ctx}")
     # turn to json serializable.
     msg["Notes"] = [i.asdict() for i in output.notes]
     msg["Rests"] = [i.asdict() for i in output.rests]
