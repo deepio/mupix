655651f1518d98a01b6ebdc6825298d9023d0e1a
deepio <alex.daigle2@gmail.com>
2020-01-30 14:45:11 -0500

    Update: version

--- mupix/__init__.py
+++ mupix/__init__.py
@@ -6,4 +6,4 @@ from .commands import cli # noqa F401
 # with open("../pyproject.toml") as f:
 #   pattern = f"(?<=version = \")(.*?)(?=\")"
 #   __version__ = re.findall(pattern, f.read(), re.DOTALL)[0]
-__version__ = '0.1.0'
+__version__ = '0.2.0'
