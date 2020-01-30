7a34674dc35c1a04fb8c2cc045f7c62fc5008401
deepio <alex.daigle2@gmail.com>
2020-01-30 14:56:11 -0500

    Fixed: Version test

--- mupix/__init__.py
+++ mupix/__init__.py
@@ -1,9 +1,8 @@
-import re
-
+# import re
 from .commands import cli # noqa F401
 
 # # Read version from toml file?
 # with open("../pyproject.toml") as f:
 #   pattern = f"(?<=version = \")(.*?)(?=\")"
 #   __version__ = re.findall(pattern, f.read(), re.DOTALL)[0]
-__version__ = '0.2.0'
+__version__ = '0.2.1'
