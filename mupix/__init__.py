dd91621e17e071265a37daabdb5c0df8f6962dd3
deepio <alex.daigle2@gmail.com>
2020-01-30 19:17:45 -0500

    Cleanup init.py

--- mupix/__init__.py
+++ mupix/__init__.py
@@ -1,8 +1,3 @@
-# import re
 from .commands import cli # noqa F401
 
-# # Read version from toml file?
-# with open("../pyproject.toml") as f:
-#   pattern = f"(?<=version = \")(.*?)(?=\")"
-#   __version__ = re.findall(pattern, f.read(), re.DOTALL)[0]
 __version__ = '0.2.1'
